615651a40a5627d800c4281f2ee7d77a4b43579d
Zack Evans <zackevans353@gmail.com>
2021-10-24 16:08:22 +0100

    File read

--- Service/src/routes/files.ts
+++ Service/src/routes/files.ts
@@ -1,32 +1,40 @@
 import * as express from "express";
 import * as formidable from "formidable";
-import { existsSync, mkdirSync, rename } from "fs";
+import {
+  existsSync,
+  mkdirSync,
+  readdir,
+  readdirSync,
+  rename,
+  statSync,
+} from "fs";
 import URLs from "../data/URLs";
+import LogManager from "../managers/LogManager";
+import { FileFilter } from "../types/Filters";
 const FileRouter = express.Router();
 
-/* GET home page. */
 FileRouter.post("/", async function (req, res, next) {
-  let dir = "/"
+  let dir = "/";
 
   const form = formidable({
     multiples: true,
-    uploadDir: URLs.DIR.User(0)
+    uploadDir: URLs.DIR.User(0),
   });
 
-  form.on('field', (name, val) => {
+  form.on("field", (name, val) => {
     switch (name) {
-      case 'dir':
+      case "dir":
         dir = val;
         break;
     }
   });
 
-  form.on('file', (field, file) => {
+  form.on("file", (field, file) => {
     const path = `${URLs.DIR.User(0)}${dir}`;
     if (!existsSync(path)) {
       mkdirSync(path);
     }
-    rename(file.path, `${path}${file.name}`, () => { });
+    rename(file.path, `${path}${file.name}`, () => {});
   });
 
   form.parse(req, (err, fields, files) => {
@@ -34,8 +42,21 @@ FileRouter.post("/", async function (req, res, next) {
       next(err);
       return;
     }
-    res.json({ fields, files });
+    res.send({ fields, files });
   });
+});
+
+FileRouter.get("/", async function (req, res, next) {
+  const query = req.body as FileFilter;
+  const path = `${URLs.DIR.User(0)}${query.dir}`;
+  let files: string[] = [],
+    folders: string[] = [];
+
+  readdirSync(path).forEach((e) => {
+    statSync(`${path}${e}`).isFile() ? files.push(e) : folders.push(e);
+  });
+
+  res.send({ files: files, folders: folders });
 });
 
 export default FileRouter;
